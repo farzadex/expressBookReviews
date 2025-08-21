239dbd1ced6d1c1a5fb83b01c44725d514aa547d
“farzadex” <f.zoghi@outlook.com>
2025-08-21 13:31:14 -0400

    + Task 2 and Added ISBN to books db

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,8 +20,12 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req?.params.isbn;
+    const booksList = Object.keys(books).map((id) => {
+        return {id: id, ...books[id]};
+    });
+    filteredBooks = booksList.filter((book) => (book.isbn === isbn));
+    res.status(200).json(filteredBooks);
 });
 
 // Get book details based on author
