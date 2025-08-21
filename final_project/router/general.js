1ce2c3df60f3d7f28306d071c3d2c232e4ce8c21
“farzadex” <f.zoghi@outlook.com>
2025-08-21 14:14:05 -0400

    + Task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,16 +4,30 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const userExists = (username) => {
+    const filtered_users = users.filter((u) => (u.username === username));
+    return (filtered_users.length > 0);
+}
 
 public_users.post("/register", (req, res) => {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    if (username && password) {
+        if (!userExists(username)) {
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registered" });
+        } else {
+            return res.status(404).json({ message: "User already exists" });
+        }
+    }
+    return res.status(404).json({ message: "Unable to register user. username/password missing" });
 });
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
     const booksList = Object.keys(books).map((id) => {
-        return {id: id, ...books[id]};
+        return { id: id, ...books[id] };
     });
     res.status(200).send(JSON.stringify(booksList, null, 4));
 });
@@ -25,7 +39,7 @@ public_users.get('/isbn/:isbn', function (req, res) {
     if (book) {
         res.status(200).json(book);
     } else {
-        res.status(404).json({message: "ISBN not found"});
+        res.status(404).json({ message: "ISBN not found" });
     }
 });
 
@@ -33,7 +47,7 @@ public_users.get('/isbn/:isbn', function (req, res) {
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
     const booksList = Object.keys(books).map((id) => {
-        return {id: id, ...books[id]};
+        return { id: id, ...books[id] };
     });
     filteredBooks = booksList.filter((book) => (book.author === author));
     res.status(200).json(filteredBooks);
@@ -43,7 +57,7 @@ public_users.get('/author/:author', function (req, res) {
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
     const booksList = Object.keys(books).map((id) => {
-        return {id: id, ...books[id]};
+        return { id: id, ...books[id] };
     });
     filteredBooks = booksList.filter((book) => (book.title === title));
     res.status(200).json(filteredBooks);
@@ -56,7 +70,7 @@ public_users.get('/review/:isbn', function (req, res) {
     if (book) {
         res.status(200).json(book.reviews);
     } else {
-        res.status(404).json({message: "ISBN not found"});
+        res.status(404).json({ message: "ISBN not found" });
     }
 });
 
